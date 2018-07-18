d3994b966cccce5bbc3da8ceb9fd2797e07684da
Bihan Viranga <bihanviranga@gmail.com>
2018-07-18 12:32:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -99,9 +99,11 @@ def winner(board)
 end
 
 def play(board)
-  puts "Make your move: "
-  over?(board)
-  value = gets.strip
-  value = input_to_index(value)
-  move(board,value,current_player(board))
+  loop do
+    puts "Make your move: "
+    over?(board)
+    value = gets.strip
+    value = input_to_index(value)
+    move(board,value,current_player(board))
+  end
 end
